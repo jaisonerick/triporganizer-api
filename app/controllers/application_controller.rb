e5227b8376eb4995c1f16e197e1b95558144b1f0
Jaison Erick <jaisonreis@gmail.com>
2017-02-27 15:59:09 -0300

    Fix admin layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,12 @@
 class ApplicationController < ActionController::Base
-  layout :layout_by_resource
-
   protect_from_forgery with: :exception
+  layout 'admin'
 
   def after_sign_out_path_for(scope)
     if scope == :admin
       admin_root_path
     else
       root_path
-    end
-  end
-
-  def layout_by_resource
-    if devise_controller? && resource_name == :admin
-      'admin'
     end
   end
 end
