8198a51b7b711c65ba34ee2315da16f031e49730
Jaison Erick <jaisonreis@gmail.com>
2017-02-28 21:51:50 -0300

    Fix hotel reservation

--- app/models/hotel_reservation.rb
+++ app/models/hotel_reservation.rb
@@ -2,5 +2,5 @@ class HotelReservation < ApplicationRecord
   belongs_to :registration
   belongs_to :destination
 
-  validates :registration, :destination, :starts_at, :ends_at, presence: true
+  validates :registration, :destination, presence: true
 end
