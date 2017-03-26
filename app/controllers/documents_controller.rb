4062a5cb12f997ff7e295889a9c572fb59b26eef
Jaison Erick <jaisonreis@gmail.com>
2017-03-26 18:06:46 -0300

    Only display documents if exists

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,5 +1,6 @@
 class DocumentsController < AuthenticatedController
   def index
     @trip = current_user.trips.visible.find(params[:trip_id])
+    @registration = @trip.registrations.find_by!(user: current_user)
   end
 end
