db751f07f215d43f4e0222fb917d5c1fbd783a44
Jaison Erick <jaisonreis@gmail.com>
2017-05-03 07:54:45 -0300

    Change suggestion behavior

--- app/models/suggestion_appointment.rb
+++ app/models/suggestion_appointment.rb
@@ -2,46 +2,9 @@ class SuggestionAppointment < Appointment
   store_accessor :details,
                  :period, :name
 
-  validates :company, :period, :description, :name, presence: true
-
-  before_validation :fix_scheduled_at
-
-  def fix_scheduled_at
-    unless scheduled_at.respond_to?(:change) && period.present?
-      self.scheduled_at = scheduled_at
-      return
-    end
-
-    self.scheduled_at = case period
-                        when 'morning'
-                          scheduled_at.change(hour: 12, min: 0, sec: 0)
-                        when 'afternoon'
-                          scheduled_at.change(hour: 18, min: 0, sec: 0)
-                        when 'night'
-                          scheduled_at.change(hour: 23, min: 59, sec: 59)
-                        else
-                          scheduled_at
-                        end
-  end
-
-  def medium
-    case period
-    when 'morning'
-      'MANHÃ LIVRE'
-    when 'afternoon'
-      'TARDE LIVRE'
-    when 'night'
-      'NOITE LIVRE'
-    else
-      'LIVRE'
-    end
-  end
+  validates :description, :name, presence: true
 
   def medium_image
     'suggestion.png'
-  end
-
-  def formatted_time
-    nil
   end
 end
