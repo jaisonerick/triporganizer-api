d66f6318f8ab0ac61ca025d1282fe4458c1e8536
Jaison Erick <jaisonreis@gmail.com>
2017-05-03 22:31:34 -0300

    Fix filename generators

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -10,6 +10,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   def filename
-    "#{DateTime.current.to_i}_#{original_filename}" if original_filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
   end
 end
--- app/uploaders/pdf_document_uploader.rb
+++ app/uploaders/pdf_document_uploader.rb
@@ -8,6 +8,13 @@ class PdfDocumentUploader < CarrierWave::Uploader::Base
   end
 
   def filename
-    "#{model.id}.pdf"
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
   end
 end
